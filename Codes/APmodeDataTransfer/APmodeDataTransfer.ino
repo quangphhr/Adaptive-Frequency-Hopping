ae774eae23e35a0d707ff2cc3774c48f3eb109da
mortalrahu <32536997+mortalrahu@users.noreply.github.com>
2019-06-17 18:35:34 +0200

    Channel Setup, String at once, all working

--- Codes/APmodeDataTransfer/APmodeDataTransfer.ino
+++ Codes/APmodeDataTransfer/APmodeDataTransfer.ino
@@ -1,38 +1,3 @@
-///
-/// @mainpage	CreateAPModeWebServer
-///
-/// @details	Start WiFi in AP mode and a basic server
-/// @n
-/// * Start WiFi in in AP mode creating a network that you can connecto to
-/// from a PC or SmartPhone. There is a limitation that only ONE device
-/// can be connected. Trying to connect to the network with a second client
-/// will fail. To connect with a different device, first disconnect the device
-/// currently connected.
-///
-///  * Display a basic web-server to turn the LEDs on and off.
-///
-/// @n
-/// @n @a		Developed with [embedXcode+](http://embedXcode.weebly.com)
-///
-/// @author		Robert Wessels
-/// @date		Mar 08, 2016
-/// @version	201
-///
-/// @copyright	This example code is in the public domain.
-///
-///
-/// @b  Hardware
-/// * CC3200 LaunchPad or MSP430/TivaC LaunchPad with CC3200 BoosterPack
-/// * Serical console
-///
-/// @b  History
-/// * Created November 2014 by Robert Wessels
-/// This example code is in the public domain.
-///
-/// * Updated 2016-03-08 by Rei Vilo
-/// Added basic web-server to turn the LEDs on and off.
-///
-
 
 // Core library for code-sense - IDE-based
 #include "Energia.h"
@@ -52,8 +17,8 @@
 // Define variables and constants
 char wifi_name[] = "energia";
 char wifi_password[] = "launchpad";
-String MSH = "Hallo, Client!!";
-int maxMSH = 1000;
+String MSH = "Hallo, Client!";
+int maxMSH = 10000;
 int count = 0;
 int port = 5000;
 
@@ -69,37 +34,32 @@ void setup()
     delay(500);
     
     Serial.println("*** LaunchPad CC3200 WiFi Web-Server in AP Mode");
-    
+
     // Start WiFi and create a network with wifi_name as the network name
     // with wifi_password as the password.
     Serial.print("Starting AP... ");
+    
     WiFi.beginNetwork(wifi_name, wifi_password);
+    int channel = 6;
+    sl_WlanSet(SL_WLAN_CFG_AP_ID, WLAN_AP_OPT_CHANNEL, 1, (unsigned char *) &channel);
     while (WiFi.localIP() == INADDR_NONE)
     {
         // print dots while we wait for the AP config to complete
         Serial.print('.');
         delay(300);
     }
-    Serial.println("DONE");
-    
+    Serial.println("DONE");    
     Serial.print("LAN name = ");
     Serial.println(wifi_name);
     Serial.print("WPA password = ");
     Serial.println(wifi_password);
-        
-//    pinMode(RED_LED, OUTPUT);      // set the LED pin mode
-//    digitalWrite(RED_LED, LOW);
     pinMode(GREEN_LED, OUTPUT);      // set the LED pin mode
     digitalWrite(GREEN_LED, LOW);
-//    pinMode(YELLOW_LED, OUTPUT);      // set the LED pin mode
-//    digitalWrite(YELLOW_LED, LOW);
-    
     IPAddress ip = WiFi.localIP();
     Serial.print("Webserver IP address = ");
-    Serial.println(ip);
-    
+    Serial.println(ip);  
     Serial.print("Web-server port = ");
-    myServer.begin();                           // start the web server on port 23
+    myServer.begin();                           // start the web server on port 5000
     Serial.println(port);
     Serial.println();
 }
@@ -142,99 +102,19 @@ void loop()
     if (myClient)
     {                             // if you get a client,
         Serial.println(". Client connected to server");           // print a message out the serial port
-        if(count < maxMSH){
+        while(count < maxMSH){
     //      if (myClient.available()){
-            myClient.println(MSH);
-            Serial.println(" Sent"+MSH +"to the client");
+            myClient.print(MSH);
+            Serial.println(" Sent "+ MSH +" to the client");
             count ++;
+            delay(10);
     //        }         
           }
-       
-//        char buffer[150] = {0};                 // make a buffer to hold incoming data
-//        int8_t i = 0;
-//        while (myClient.connected())
-//        {            // loop while the client's connected
-//            if (myClient.available())
-//            {             // if there's bytes to read from the client,
-//                char c = myClient.read();             // read a byte, then
-//                Serial.write(c);                    // print it out the serial monitor
-//                if (c == '\n') {                    // if the byte is a newline character
-//                    
-//                    // if the current line is blank, you got two newline characters in a row.
-//                    // that's the end of the client HTTP request, so send a response:
-//                    if (strlen(buffer) == 0)
-//                    {
-//                        // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-//                        // and a content-type so the client knows what's coming, then a blank line:
-//                        myClient.println("HTTP/1.1 200 OK");
-//                        myClient.println("Content-type:text/html");
-//                        myClient.println();
-//                        
-//                        // the content of the HTTP response follows the header:
-//                        myClient.println("<html><head><title>Energia CC3200 WiFi Web-Server in AP Mode</title></head><body align=center>");
-//                        myClient.println("<h1 align=center><font color=\"red\">LaunchPad CC3200 WiFi Web-Server in AP Mode</font></h1>");
-//                        myClient.print("Red LED <button onclick=\"location.href='/RH'\">HIGH</button>");
-//                        myClient.println(" <button onclick=\"location.href='/RL'\">LOW</button><br>");
-//                        myClient.print("Green LED <button onclick=\"location.href='/GH'\">HIGH</button>");
-//                        myClient.println(" <button onclick=\"location.href='/GL'\">LOW</button><br>");
-//                        myClient.print("Yellow LED <button onclick=\"location.href='/YH'\">HIGH</button>");
-//                        myClient.println(" <button onclick=\"location.href='/YL'\">LOW</button><br>");
-//                        
-//                        // The HTTP response ends with another blank line:
-//                        myClient.println();
-//                        // break out of the while loop:
-//                        break;
-//                    }
-//                    else
-//                    {      // if you got a newline, then clear the buffer:
-//                        memset(buffer, 0, 150);
-//                        i = 0;
-//                    }
-//                }
-//                else if (c != '\r')
-//                {    // if you got anything else but a carriage return character,
-//                    buffer[i++] = c;      // add it to the end of the currentLine
-//                }
-//                
-//                Serial.println();
-//                String text = buffer;
-//                // Check to see if the client request was "GET /H" or "GET /L":
-//                if (text.endsWith("GET /RH"))
-//                {
-//                    digitalWrite(RED_LED, HIGH);               // GET /H turns the LED on
-//                    Serial.println(". RED_LED on");
-//                }
-//                if (text.endsWith("GET /RL"))
-//                {
-//                    digitalWrite(RED_LED, LOW);                // GET /L turns the LED off
-//                    Serial.println(". RED_LED off");
-//                }
-//                if (text.endsWith("GET /GH"))
-//                {
-//                    digitalWrite(GREEN_LED, HIGH);               // GET /H turns the LED on
-//                    Serial.println(". GREEN_LED on");
-//                }
-//                if (text.endsWith("GET /GL"))
-//                {
-//                    digitalWrite(GREEN_LED, LOW);                // GET /L turns the LED off
-//                    Serial.println(". GREEN_LED off");
-//                }
-//                if (text.endsWith("GET /YH"))
-//                {
-//                    digitalWrite(YELLOW_LED, HIGH);               // GET /H turns the LED on
-//                    Serial.println(". YELLOW_LED on");
-//                }
-//                if (text.endsWith("GET /YL"))
-//                {
-//                    digitalWrite(YELLOW_LED, LOW);                // GET /L turns the LED off
-//                    Serial.println(". YELLOW_LED off");
-//                }
-//            }
-//        }
-        
-        // close the connection:
-//        myClient.stop();
-//        Serial.println(". Client disconnected from server");
-//        Serial.println();
+    // close the connection:
+        myClient.stop();
+        Serial.println(" Client disconnected from server ");
+        Serial.println();
+        digitalWrite(GREEN_LED, LOW);             
     }
+   
 }
--- Codes/Socket/socket_client.py
+++ Codes/Socket/socket_client.py
@@ -2,7 +2,23 @@
 # the main function is to receive data from server and print it.
 # can make it an echo client by removing comment line
 import socket
- 
+buff = []
+# Python program to convert a list 
+# of character 
+
+def convert(s): 
+
+	# initialization of string to "" 
+	new = "" 
+
+	# traverse in the string 
+	for x in s: 
+		new += x 
+
+	# return string 
+	return new 
+	
+
 def Main():
             # 127.0.0.1 = local machine.
             # change to the address of the server accordingly
@@ -14,11 +30,15 @@ def Main():
         mySocket.connect((host,port))
          
         while True:
-                data = mySocket.recv(1024).decode()
+                data = mySocket.recv(1).decode()
                 if not data:
                         break
-                print ('Received from server: ' + str(data))
-                 
+                if data != '!':
+                        buff.append(data)
+                elif data == '!' :
+                        buff.append(data)
+                        print ('Received from server: ' + convert(buff))
+                        buff.clear()
                 #data = str(data).upper()
                 #print ("echo: " + str(data))
                 #mySocket.send(data.encode())
