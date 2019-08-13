ec51af0034b25eb74d4215f80598b06c4ce84d8a
Hopel.ess <32953667+quangphhr@users.noreply.github.com>
2019-08-13 11:07:49 +0200

    Add files via upload

--- Codes/Socket/socket_client.py
+++ Codes/Socket/socket_client.py
@@ -1,10 +1,12 @@
-# a simple socket client
+# a reconnect socket client.
 # the main function is to receive data from server and print it.
-# can make it an echo client by removing comment line
+# manage stream of characters by detecting eol.
+
 import socket
+from time import sleep
+#from subprocess import check_output
+
 buff = []
-# Python program to convert a list 
-# of character 
 
 def convert(s): 
 
@@ -20,30 +22,50 @@ def convert(s):
 	
 
 def Main():
-            # 127.0.0.1 = local machine.
-            # change to the address of the server accordingly
+        # 127.0.0.1 = local machine.
+        # change to the address of the server accordingly
         host = "192.168.1.1"
-            # port should be >1024
+        # port should be >1024
         port = 5000
          
         mySocket = socket.socket()
-        mySocket.connect((host,port))
+        connected = False
+
          
         while True:
-                data = mySocket.recv(1).decode()
-                if not data:
-                        break
-                if data != '!':
-                        buff.append(data)
-                elif data == '!' :
-                        buff.append(data)
-                        print ('Received from server: ' + convert(buff))
-                        buff.clear()
+                if(not connected):
+                        try:
+                                mySocket.connect((host,port))
+                                connected = True
+                                sleep(10)
+                        except:
+                                pass
+                else:
+                        try:
+                                data = mySocket.recv(1).decode()
+                                if not data:
+                                       break
+                                if data != '!':
+                                       buff.append(data)
+                                else:
+                                       buff.append(data)
+                                       print ('Received from server: ' + convert(buff))
+                                       #print ('Received from channel' + channels.index(current_freq) + ' :' + convert(buff))
+                                       buff.clear()
+                                #print ('Received from server: ' + convert(data))
+
+                        except:
+                                mySocket = socket.socket()
+                                connected = False
+                                pass
+                        #time.sleep(1)
                 #data = str(data).upper()
                 #print ("echo: " + str(data))
                 #mySocket.send(data.encode())
                  
         mySocket.close()
- 
-if __name__ == '__main__':
+
+while(1): 
+#if __name__ == '__main__':
     Main()
+    sleep(10)
